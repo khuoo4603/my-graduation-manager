0491dfd45fc1dbdba960db9754baff3b3d1263b8
khuoo <khuoo4603@gmail.com>
2026-02-12 18:26:11 +0000

    chore: 초기 프로젝트 구조 추가 및 .gitignore 정리

---
+++ backend/src/main/java/com/khuoo/gradmanager/GradmanagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.khuoo.gradmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradmanagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradmanagerBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/khuoo/gradmanager/GradmanagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khuoo.gradmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradmanagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
