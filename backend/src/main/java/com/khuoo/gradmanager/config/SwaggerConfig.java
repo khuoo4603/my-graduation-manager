b7ef81eefcf794c7c678bc2ab76c435d3dce3ddd
khuoo <khuoo4603@users.noreply.github.com>
2026-03-05 00:10:09 +0900

    fix: [backend] swagger 호출 url고정  - http -> https

--- backend/src/main/java/com/khuoo/gradmanager/config/SwaggerConfig.java
+++ backend/src/main/java/com/khuoo/gradmanager/config/SwaggerConfig.java
@@ -2,30 +2,47 @@ package com.khuoo.gradmanager.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
 public class SwaggerConfig {
+    @Value("${app.swagger.server-url:https://api.khuoo.synology.me}")
+    private String serverUrl;
 
     @Bean
     public OpenAPI openAPI() {
-        String jwtSchemeName = "BearerAuth";
+
+        String cookieSchemeName = "CookieAuth";
 
         return new OpenAPI()
-                .components(new Components()
-                        .addSecuritySchemes(jwtSchemeName,
-                                new SecurityScheme()
-                                        .name("Authorization")
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")))
-                .addSecurityItem(new SecurityRequirement().addList(jwtSchemeName))
-                .info(new Info().title("My Graduation Manager API")
-                        .description("API Documentation")
-                        .version("v1"));
+                // Swagger가 호출할 서버 고정
+                .servers(List.of(new Server().url(serverUrl)))
+
+                // authorizations 설명 추가
+                .components(new Components().addSecuritySchemes(cookieSchemeName,
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.APIKEY)
+                                .in(SecurityScheme.In.COOKIE)
+                                .name("access_token")
+                ))
+
+                .addSecurityItem(new SecurityRequirement().addList(cookieSchemeName))
+
+                // 문서 메타데이터
+                .info(new Info()
+                        .title("My Graduation Manager API")
+                        .description("".trim())
+                        .version("v1")
+                        .contact(new Contact().name("khuoo"))
+                );
     }
 }
